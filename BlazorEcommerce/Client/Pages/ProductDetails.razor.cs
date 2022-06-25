f4efeb10592550d4fe1963cf49c72fbf4ffb7f2b
leiyao <C-primer@outlook.com>
2022-06-25 21:30:27 +0800

    Add ProductDetails

---
+++ BlazorEcommerce/Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Pages
+{
+    public partial class ProductDetails
+    {
+        private Product? product = null;
+
+        [Parameter]
+        public int Id { get; set; }
+        protected override async Task OnParametersSetAsync()
+        {
+            product = ProductService.Products.Find(p => p.Id == Id);
+        }
+
+    }
+}
